9de793ecd922416ac01d32c22d51be779dda1fb0
taher-cc <vc.taher.lakdawa@zee.com>
2023-11-21 21:21:50 +0530

    package added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module translator
+
+go 1.20
+
+require github.com/antlr/antlr4/runtime/Go/antlr v1.4.10
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+
+	// Import the generated lexer and parser packages
+	parser "./parser" // Replace with the actual package name of your generated files
+)
+
+func main() {
+	// Your CQL query
+	input := "SELECT * FROM table_name WHERE condition;"
+
+	// Create an input stream of the query
+	lexer := parser.NewCQLLexer(antlr.NewInputStream(input))
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+
+	// Create the parser with the token stream
+	p := parser.NewCQLParser(stream)
+
+	// Parse the input and get the parse tree
+	tree := p.Start()
+
+	// Do something with the parse tree (e.g., traverse or interpret)
+	// For example, you might traverse the tree to perform actions based on CQL syntax.
+
+	fmt.Println(tree.ToStringTree(nil, p))
+	// Above line prints the parse tree representation (for demonstration purposes)
+}
